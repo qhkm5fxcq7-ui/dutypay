90e8eb06ffc6034aa9905334350e885c0a583b43
Manuel Mazzola <manuelmazzola@MAC-54BC68.station>
2026-06-25 16:18:48 +0200

    refactor: rename Break repository to room repository

---
+++ lib/features/break/domain/repositories/break_room_repository.dart
@@ -0,0 +1,28 @@
+import '../models/break_participant.dart';
+import '../models/break_room.dart';
+
+abstract interface class BreakRoomRepository {
+  Future<BreakRoom> createRoom({
+    String? title,
+  });
+
+  Future<BreakRoom> joinRoom({
+    required String roomCode,
+    String? nickname,
+  });
+
+  Stream<BreakRoom?> watchRoom(String roomId);
+
+  Stream<List<BreakParticipant>> watchParticipants(
+    String roomId,
+  );
+
+  Future<void> setReady({
+    required String roomId,
+    required bool isReady,
+  });
+
+  Future<void> startRound(String roomId);
+
+  Future<void> resetRound(String roomId);
+}
