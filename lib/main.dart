fbf40a8499fd0773456a9466f4f1310cac818ab9
Manuel Mazzola <manuelmazzola@MAC-54BC68.station>
2026-06-26 14:21:26 +0200

    fix: use Android Firebase app options

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,11 @@ Future<void> main() async {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
         apiKey: 'AIzaSyAes2ptObzGaej5sNI5WoOkwTqSI0iZx_U',
-        appId: '1:1069881451179:ios:85c0f7fb753eaf195c4dee',
+        appId: '1:1069881451179:android:42bca9f5105e812b5c4dee',
         messagingSenderId: '1069881451179',
         projectId: 'dutypay-ec498',
         storageBucket: 'dutypay-ec498.firebasestorage.app',
-        iosBundleId: 'com.dutypay.app',
-      ),
+        ),
     );
 
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
