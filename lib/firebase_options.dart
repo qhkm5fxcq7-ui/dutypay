0e2f980518626e45f8015982f7e3ff362f1ce0be
Manuel Mazzola <manuelmazzola@MAC-54BC68.station>
2026-06-28 12:11:44 +0200

    chore: configure Firebase web for Break testing

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,7 +5,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError('Firebase web options are not configured.');
+      return web;
     }
 
     switch (defaultTargetPlatform) {
@@ -13,12 +13,24 @@ class DefaultFirebaseOptions {
         return android;
       case TargetPlatform.iOS:
         return ios;
+      case TargetPlatform.macOS:
+        return macos;
       default:
         throw UnsupportedError(
           'Firebase options are not configured for this platform.',
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDusFxoYL2MWiWBDj-RD9vwFXAkdcLwS4Q',
+    authDomain: 'dutypay-ec498.firebaseapp.com',
+    projectId: 'dutypay-ec498',
+    storageBucket: 'dutypay-ec498.firebasestorage.app',
+    messagingSenderId: '1069881451179',
+    appId: '1:1069881451179:web:b619adc134c6b2225c4dee',
+    measurementId: 'G-2F59KS1LY2',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyBspdeGCfiVB8qgSNZ-o9SOqSQC8omd2Dk',
@@ -29,6 +41,15 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyAes2ptObzGaej5sNI5WoOkwTqSI0iZx_U',
+    appId: '1:1069881451179:ios:85c0f7fb753eaf195c4dee',
+    messagingSenderId: '1069881451179',
+    projectId: 'dutypay-ec498',
+    storageBucket: 'dutypay-ec498.firebasestorage.app',
+    iosBundleId: 'com.dutypay.app',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
     apiKey: 'AIzaSyAes2ptObzGaej5sNI5WoOkwTqSI0iZx_U',
     appId: '1:1069881451179:ios:85c0f7fb753eaf195c4dee',
     messagingSenderId: '1069881451179',
